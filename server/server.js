55c36f3c0150b6fa5742f3092863bf2748a1b911
Johnson Yang <johnsonyang65@gmail.com>
2024-03-18 21:11:51 -0700

    connected

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-
-
-
+ 
 
 // Load environment variables from .env file
 require('dotenv').config();
