b90d964a057f572e6070c0433a75fe397722ca09
Johnson Yang <johnsonyang65@gmail.com>
2024-03-21 21:26:39 -0700

    cleaned up code

--- server/server.js
+++ server/server.js
@@ -37,46 +37,6 @@ app.use('/users', userRoutes); // Mount user routes at /users
 app.use('/recipes', recipeRoutes); // Mount recipe routes at /recipes
 
 
-
-
-
-  // Define sample recipe data
-const sampleRecipeData = {
-    username: 'john_doe',
-    recipeName: 'Spaghetti Carbonara',
-    inspiration: 'Italian cuisine',
-    specialtyDiets: 'None',
-    categories: 'Pasta',
-    serves: 4,
-    recipeImage: 'https://example.com/spaghetti-carbonara.jpg',
-    prepTime: { hours: 0, minutes: 15 },
-    cookTime: { hours: 0, minutes: 20 },
-    totalTime: { hours: 0, minutes: 35 },
-    ingredients: [
-        { quantity: '200', unit: 'g', name: 'spaghetti' },
-        { quantity: '150', unit: 'g', name: 'guanciale or pancetta' },
-        // Add more ingredients as needed
-    ],
-    instructions: '...'
-};
-
-// Insert sample data into MongoDB
-async function insertSampleData() {
-    try {
-        const newRecipe = new Recipe(sampleRecipeData);
-        await newRecipe.save();
-        console.log('Sample data inserted successfully');
-    } catch (error) {
-        console.error('Error inserting sample data:', error);
-    } 
-}
-
-// Call the function to insert sample data
-insertSampleData();
-
-
-   
-   
   
    
 // Error handling middleware for undefined routes
