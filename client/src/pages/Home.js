5c8a1913fbdc346d4f332445c797119c085a24ea
Johnson Yang <johnsonyang65@gmail.com>
2024-03-23 09:50:32 -0700

    updated home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
 
     return (
         <div className="main-content">
-            {/* Recipe Preview Section */}
+            {/* Recipe Preview Sectionnnn */}
             <div className="recipe-preview">
                 <h2>Recently Submitted Recipes</h2>
                 {/* Display recent recipes here */}
@@ -36,19 +36,19 @@ function Home() {
                         <h3>Specialty</h3>
                         <div className="content">
                             <ul>
-                                <li>Vegan</li>
-                                <li>Gluten-Free</li>
-                                <li>Dairy-Free</li>
-                                <li>Grain-Free</li>
-                                <li>Egg-Free</li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Vegan')}>Vegan</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Gluten-Free')}>Gluten-Free</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Dairy-Free')}>Dairy-Free</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Grain-Free')}>Grain-Free</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Egg-Free')}>Egg-Free</Link></li>
                             </ul>
                         </div>
                         <h3>Meal Times</h3>
                         <div className="content">
                             <ul>
-                                <li><Link to="/recipedisplay">Breakfast</Link></li>
-                                <li>Brunch</li>
-                                <li>Lunch</li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Breakfast')}>Breakfast</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Brunch')}>Brunch</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Lunch')}>Lunch</Link></li>
                             </ul>
                         </div>
                     </div>
@@ -56,19 +56,19 @@ function Home() {
                         <h3>Main dishes</h3>
                         <div className="content">
                             <ul>
-                                <li>Beef</li>
-                                <li>Poultry</li>
-                                <li>Pork</li>
-                                <li>Seafood</li>
-                                <li>Vegetarian</li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Beef')}>Beef</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Poultry')}>Poultry</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Pork')}>Pork</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Seafood')}>Seafood</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Vegetarian')}>Vegetarian</Link></li>
                             </ul>
                         </div>
                         <h3>Side Dishes</h3>
                         <div className="content">
                             <ul>
-                                <li>Vegetables</li>
-                                <li>Grain</li>
-                                <li>Other</li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Vegetables')}>Vegetables</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Grain')}>Grain</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Other')}>Other</Link></li>
                             </ul>
                         </div>
                         {/* Add more categories as needed */}
@@ -77,19 +77,16 @@ function Home() {
                         <h3>Desserts</h3>
                         <div className="content">
                             <ul>
-                                <li>Cookies</li>
-                                <li>Bars</li>
-                                <li>Cakes/Frosting</li>
-                                <li>Pies</li>
-                                <li>Candy</li>
-                                <li>Bread</li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Cookies')}>Cookies</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Bars')}>Bars</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Cakes/Frosting')}>Cakes/Frosting</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Pies')}>Pies</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Candy')}>Candy</Link></li>
+                                <li><Link onClick={() => fetchRecipesByCategory('Bread')}>Bread</Link></li>
                             </ul>
                         </div>
                         <h3>Beverages</h3>
-                        <ul>
-                        <li>For Minors 🧃</li>
-                        <li>For Miners 🍹⛏️</li>
-                        </ul>
+                        {/* Add more categories as needed */}
                     </div>
                 </div>
             </div>
